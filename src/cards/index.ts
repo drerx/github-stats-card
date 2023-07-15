c11b13e45f7003610d4be038a78adbe7bc675daa
drerx <119292904+drerx@users.noreply.github.com>
2023-07-15 19:54:39 +0300

    Add files via upload

---
+++ src/cards/index.ts
@@ -0,0 +1,5 @@
+export { default as UserCard } from "./userCard";
+export { default as RepoCard } from "./repoCard";
+export { default as LangCard } from "./langCard";
+export { default as ErrorCard } from "./errorCard";
+export { default as UserDeepCard } from "./userDeepCard";
